e6a59a4902d7957afbdb806c5675c67ef054888b
yendelevium <yashbardia27@gmail.com>
2025-02-25 01:14:29 +0530

    Solve Conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import Footer from "./components/Footer";
 import ImageSlider from "./components/ImageSlider";
 import Header from "./components/Header";
 import Cards from "./components/Cards";
-import Convenors from "./components/Convenors";
 import slider_1 from "./images/Campus1.jpg";
 import slider_2 from "./images/Campus2.jpg";
 import slider_3 from "./images/Campus3.jpg";
 import slider_4 from "./images/Campus4.jpg";
 import AboutCampus from "./components/AboutCampus";
-import Header from "./components/Header";
 
 function App() {
     const images = [slider_1, slider_2, slider_3, slider_4];
