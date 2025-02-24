dee985618021c7b795eb21051a1ac23111729fa3
priyansh-narang2308 <145008344+priyansh-narang2308@users.noreply.github.com>
2025-02-24 10:18:57 +0530

    made convenors section

---
+++ src/Convenors.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Convenors.css';
+
+const Convenors = () => {
+    return (
+        <div className="convenors-container">
+            <h1 className="convenors-title">OUR CONVENORS</h1>
+
+            <div className="convenors-wrapper">
+                <div className="convenor-section">
+                    <div className="convenor-image">
+                        <img src="" alt="" />
+                    </div>
+
+                    <div className="convenor-content">
+                    
+                    </div>
+                </div>
+<hr className='divide' />
+                <div className="convenor-section right">
+                    <div className="convenor-image">
+                       <img src="" alt="" />
+                    </div>
+                    <div className="convenor-content">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Convenors;
