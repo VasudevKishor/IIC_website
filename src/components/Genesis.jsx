7026bcf2607a6867debe84782e31653601341dc9
Tarika06 <tarika200506@gmail.com>
2025-02-24 22:38:34 +0530

    changes saved

--- src/components/Genesis.jsx
+++ src/components/Genesis.jsx
@@ -2,29 +2,32 @@ import Countdown from "./Countdown";
 import back from "../images/arrow.svg";
 import iicLogo from "../images/iic-logo-notext.png";
 import genesisLogo from "../images/genesis-logo-removebg-notext.png";
+import "../styles/Genesis.css"
 
 function Genesis() {
     return (
         <>
-            <header>
+            <header className="gen-header">
                 <div className="back">
-                    <img src={back} alt="back arrow"></img>
+                    <img src={back} alt="back arrow" className="arrow" width="auto" height="15"></img>
                     Back
                 </div>
-                <div className="genesis-title">
-                    <img src={iicLogo} alt="" />
+                <center className="genesis-title">
+                    <img src={iicLogo} alt=""  width="auto" height="15"/>
                     GENESIS
-                </div>
+                </center>
             </header>
-            <div className="left">
-                <img src={genesisLogo} alt="" />
-            </div>
-            <div className="right">
-                <div className="genesis">
-                    <h1>GENESIS'25</h1>
-                    <a>Register</a>
-                    <div className="countdown">
-                        <Countdown />
+            <div className="bottom">
+                <div className="left">
+                    <img src={genesisLogo} alt="" />
+                </div>
+                <div className="right">
+                    <div className="genesis">
+                        <h1>GENESIS'25</h1>
+                        <a>Register</a>
+                        <div className="countdown">
+                            <Countdown />
+                        </div>
                     </div>
                 </div>
             </div>
