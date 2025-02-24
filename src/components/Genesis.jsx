32204ea284e4056f0e43ff9214fb76b9ced84ebc
Nandagopal Menon <nandagopalnmenon@gmail.com>
2025-02-24 23:00:26 +0530

    checkpoint

--- src/components/Genesis.jsx
+++ src/components/Genesis.jsx
@@ -2,20 +2,30 @@ import Countdown from "./Countdown";
 import back from "../images/arrow.svg";
 import iicLogo from "../images/iic-logo-notext.png";
 import genesisLogo from "../images/genesis-logo-removebg-notext.png";
-import "../styles/Genesis.css"
+import amritaEmblem from "../images/amrita-emblem.svg";
+import "../styles/Genesis.css";
 
 function Genesis() {
     return (
         <>
             <header className="gen-header">
                 <div className="back">
-                    <img src={back} alt="back arrow" className="arrow" width="auto" height="15"></img>
+                    <img
+                        src={back}
+                        alt="back arrow"
+                        className="arrow"
+                        width="auto"
+                        height="15"
+                    ></img>
                     Back
                 </div>
                 <center className="genesis-title">
-                    <img src={iicLogo} alt=""  width="auto" height="15"/>
+                    <img src={iicLogo} alt="" width="auto" height="15" />
                     GENESIS
                 </center>
+                <div className="amrita-emblem-container">
+                    <img src={amritaEmblem} alt="" className="amrita-emblem" />
+                </div>
             </header>
             <div className="bottom">
                 <div className="left">
