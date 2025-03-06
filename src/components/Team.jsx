e0d038453f2d1d3697252ec01beb1e70f3255e56
Vasudev Kishor <vasudevkishor@gmail.com>
2025-03-06 13:31:45 +0530

    Members List Updated

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -11,7 +11,16 @@ import adarsh from "../images/DrAdarshS.jpg";
 import ramraj from "../images/ramraj.jpg"
 import adithya from "../images/aditya.jpg"
 import sanyam from "../images/sanyam.jpeg"
-
+import Thilagan from "../images/IIC/DSC_0031.jpg"
+import Parvathy from "../images/IIC/DSC_0052.jpg"
+import Dhamini from "../images/IIC/DSC_0134.jpg"
+import Vasudev from "../images/IIC/DSC_0166.jpg"
+import Neeraj from "../images/IIC/DSC_0185.jpg"
+import Aakash from "../images/IIC/DSC_0576.jpg"
+import Nair from "../images/IIC/DSC_0600.jpg"
+import Ramu from "../images/IIC/DSC_0621.jpg"
+import Subhashini from "../images/IIC/DSC_8734.jpg"
+import shruthi from "/Users/vasudevkishor/Documents/IIC/WebIIC/IIC_website/src/images/PHOTO-2025-03-06-13-27-31.jpg"
 const data = [
   {
     title: "Convenors",
@@ -31,6 +40,63 @@ const data = [
     members: [
       { name: "Ramraj S", role: "Vice Chair", image: ramraj },
       { name: "Sanyam Bhardwaj", role: "Vice Chair", image: sanyam }
+    ]
+  },
+  {
+    title: "Tech and Design",
+    members: [
+      { name: "Neeraj Nair", role: "Head", image: Neeraj },
+      { name: "Vasudev Kishor", role: "Co-Head", image: Vasudev }
+    ]
+  },
+  {
+    title: "PR and Outreach",
+    members: [
+      { name: "Subhashini Sudhakar", role: "Head", image: Subhashini },
+      { name: "Thilagan Iniyavan", role: "Co-Head", image: Thilagan }
+    ]
+  },
+  {
+    title: "Fair",
+    members: [
+      { name: "Deepak Kumar S", role: "Head", image: ramraj }
+    ]
+  },
+  {
+    title: "Workshop",
+    members: [
+      { name: "Shruti Sivakumar", role: "Head", image: shruthi }
+    ]
+  },
+  {
+    title: "Exodus",
+    members: [
+      { name: "Akash Sriram", role: "Head", image: Aakash },
+      { name: "Dhamini Manoj", role: "Co-Head", image: Dhamini }
+    ]
+  },
+  {
+    title: "Logistics",
+    members: [
+      { name: "Hemadhri P C", role: "Head", image: ramraj }
+    ]
+  },
+  {
+    title: "Hospitality",
+    members: [
+      { name: "Parvathi S", role: "Head", image: Parvathy }
+    ]
+  },
+  {
+    title: "Admin",
+    members: [
+      { name: "Theerth Krish", role: "Head", image: ramraj }
+    ]
+  },
+  {
+    title: "Finance",
+    members: [
+      { name: "Shaun Joseph Sunny", role: "Head", image: ramraj }
     ]
   }
 ];
