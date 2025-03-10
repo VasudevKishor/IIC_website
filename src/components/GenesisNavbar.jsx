b5dcd4bb7a8a24555a4e94c5f0c29074893b2a5b
adofm <karthik.maniam008@gmail.com>
2025-03-10 14:55:37 +0530

    feat added hamburger to the iic website made the navbar responsive for the genisis website

--- src/components/GenesisNavbar.jsx
+++ src/components/GenesisNavbar.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FiMenu, FiX } from "react-icons/fi";
 import Amritalogo from "../images/amrita-emblem.svg";
 import IIClogo from "../images/iic-logo.png";
-import { Link } from "react-router";
 import "../styles/GenesisNavbar.css";
 
 export default function GenesisNavbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <nav className="genesis-navbar">
             <div id="logo">
@@ -13,13 +17,21 @@ export default function GenesisNavbar() {
                     id="IIClogoGen"
                 />
             </div>
-            <div className="genesis-navlinks">
-                <Link to={"/"}>Home</Link>
-                <Link to={"/genesis"}>Genesis'25</Link>
-                <a href={"#exodus"}>Events(Exodus)</a>
-                <a href={"#fair"}>Fair</a>
-                <a href={"#workshop"}>Workshops</a>
+
+            {/* Hamburger Button */}
+            <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                {isOpen ? <FiX size={30} /> : <FiMenu size={30} />}
             </div>
+
+            {/* Navigation Links */}
+            <div className={`genesis-navlinks ${isOpen ? "open" : ""}`}>
+                <Link to={"/"} onClick={() => setIsOpen(false)}>Home</Link>
+                <Link to={"/genesis"} onClick={() => setIsOpen(false)}>Genesis'25</Link>
+                <a href={"#exodus"} onClick={() => setIsOpen(false)}>Events(Exodus)</a>
+                <a href={"#fair"} onClick={() => setIsOpen(false)}>Fair</a>
+                <a href={"#workshop"} onClick={() => setIsOpen(false)}>Workshops</a>
+            </div>
+
             <div id="logo">
                 <img
                     src={Amritalogo}
