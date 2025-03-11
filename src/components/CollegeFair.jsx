2e2dc3aae14d290ebc455aa517927fbb02a2246a
Vasudev Kishor <vasudevkishor@gmail.com>
2025-03-11 12:14:04 +0530

    Link Updated

--- src/components/CollegeFair.jsx
+++ src/components/CollegeFair.jsx
@@ -10,7 +10,7 @@ export default function Exodus(){
                 The Genesis College Fair 2025 is a one-day exhibition designed to showcase innovative solutions across STEM, sustainability, entrepreneurship, and social impact. As part of the Institution’s Innovation Council (IIC) initiative under the Ministry of Education, the fair provides a platform for young innovators to present their ideas to investors, mentors, and industry leaders.  This year’s fair highlights the theme of *"Green Innovation,"* encouraging students to develop projects that address environmental challenges while promoting sustainable practices. From cutting-edge prototypes to visionary business models, the exhibition celebrates creativity and forward-thinking solutions.  
                 </p>
                 <div className="register-now">
-                    <a href="">Register Now!</a>
+                    <a href="https://forms.gle/ZswM9cFaEMJtD3ix7">Register Now!</a>
                 </div>
             </section>
            
